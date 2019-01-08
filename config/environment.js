50b9330af2898a2de2338d9c42b745ad72d99852
Buck Doyle <b@chromatin.ca>
2019-01-08 14:21:08 -0600

    Add ember-intercom-io

--- config/environment.js
+++ config/environment.js
@@ -17,6 +17,11 @@ module.exports = function(environment) {
       }
     },
 
+    intercom: {
+      appId: 'placeholder',
+      enabled: true
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
